28396e302670a800491b133708f17a895f009ce4
Eduardo Miyashiro Asato <ed.miya.a@gmail.com>
2022-07-13 21:59:09 +0200

    Use config.el instead of custom.el

--- custom.el
+++
@@ -1,9 +0,0 @@
-(load-theme 'doom-city-lights t)
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(global-set-key (kbd "<f9>") (lambda() (interactive)(find-file (concat doom-private-dir "custom.el"))))
-
-;; Load any .el files inside extra directory
-(setq extra-config-directory (expand-file-name (concat doom-private-dir "extra")))
-(if (file-directory-p extra-config-directory)
-    (mapc 'load (directory-files extra-config-directory t "\\.el$")))
