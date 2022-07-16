dc65b9839cbb6bb38b86ef0dd62dace83304b37b
Eduardo Miyashiro Asato <ed.miya.a@gmail.com>
2022-07-16 17:48:35 +0200

    Add open-file-in-clipboard function

--- config.el
+++ config.el
@@ -11,3 +11,23 @@
 (setq extra-config-directory (expand-file-name (concat doom-private-dir "extra")))
 (if (file-directory-p extra-config-directory)
     (mapc 'load (directory-files extra-config-directory t "\\.el$")))
+
+;; Custom functions
+(defun open-file-in-clipboard ()
+  "Open current clipboard content if it's a valid file path."
+  (interactive)
+  (let* ((clipboard-content (w32-get-clipboard-data))
+         (clipboard-content-clean (if clipboard-content
+                                      (replace-regexp-in-string
+                                       "['\"]+$\\|^['\"]+"
+                                       ""
+                                       clipboard-content))))
+    (if (and clipboard-content-clean (file-exists-p clipboard-content-clean))
+        (progn (message "Opening file: %s" clipboard-content-clean)
+               (find-file clipboard-content-clean))
+      (message "Invalid file: %s" clipboard-content-clean))))
+
+;; Keybindings
+(map! :leader
+      (:prefix ("z" . "custom")
+       :desc "Open file in clipboard" "o" #'open-file-in-clipboard))
